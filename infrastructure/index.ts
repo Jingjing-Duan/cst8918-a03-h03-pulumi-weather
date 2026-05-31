306391f9c1dd278eab7a1c96420f741a2af47f31
Jingjing Duan <duan0029@algonquinlive.com>
2026-05-31 18:00:57 -0400

    Initialize Pulumi project and create ACR

---
+++ infrastructure/index.ts
@@ -0,0 +1,44 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as containerregistry from "@pulumi/azure-native/containerregistry";
+
+const config = new pulumi.Config();
+
+const appPath = config.require("appPath");
+const prefixName = config.require("prefixName");
+const imageName = prefixName;
+const imageTag = config.require("imageTag");
+
+const containerPort = config.requireNumber("containerPort");
+const publicPort = config.requireNumber("publicPort");
+const cpu = config.requireNumber("cpu");
+const memory = config.requireNumber("memory");
+
+// Create a resource group
+const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`);
+
+// Create Azure Container Registry
+const acrName = `${prefixName.replace(/-/g, "")}acr`;
+
+const registry = new containerregistry.Registry(acrName, {
+  resourceGroupName: resourceGroup.name,
+  adminUserEnabled: true,
+  sku: {
+    name: containerregistry.SkuName.Basic,
+  },
+});
+
+const registryCredentials = containerregistry
+  .listRegistryCredentialsOutput({
+    resourceGroupName: resourceGroup.name,
+    registryName: registry.name,
+  })
+  .apply((creds) => {
+    return {
+      username: creds.username!,
+      password: creds.passwords![0].value!,
+    };
+  });
+
+export const acrServer = registry.loginServer;
+export const acrUsername = registryCredentials.username;
